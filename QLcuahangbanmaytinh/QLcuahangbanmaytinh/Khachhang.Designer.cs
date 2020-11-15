1fa500913c5d2778516a42d8dd05d453387c97db
ngockha2k <ngockha2kcnpm@gmail.com>
2020-11-15 11:13:51 +0700

    chuc nang qlnv

--- QLcuahangbanmaytinh/QLcuahangbanmaytinh/Khachhang.Designer.cs
+++ QLcuahangbanmaytinh/QLcuahangbanmaytinh/Khachhang.Designer.cs
@@ -222,7 +222,7 @@
             this.lbldiachi.AutoSize = true;
             this.lbldiachi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbldiachi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.lbldiachi.Location = new System.Drawing.Point(391, 41);
+            this.lbldiachi.Location = new System.Drawing.Point(427, 40);
             this.lbldiachi.Name = "lbldiachi";
             this.lbldiachi.Size = new System.Drawing.Size(56, 18);
             this.lbldiachi.TabIndex = 3;
